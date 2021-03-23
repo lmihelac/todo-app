ab131e1fb8a33a98c8efb14c5d59695a7a6637f7
Luka Mihelac <luka.mihelac7@gmail.com>
2021-03-23 15:18:25 +0100

    Added all functionality to script.js

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-const form = document.getElementById('form')
-const input = document.getElementById('input') 
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const todos = document.getElementById('todos');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    const todo = input.value;
+    const todoText = input.value;
 
-    if(todo) {
-        return 
+    if(todoText) {
+        const todoEl = document.createElement('li');
+        todoEl.innerText = todoText;
+        todos.appendChild(todoEl);
+
+        todoEl.addEventListener('click', () => {
+            todoEl.classList.toggle('completed');
+        });
+
+        input.value = '';
     }
-})
+
+});
